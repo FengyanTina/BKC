a1f72d0ffe74cd56c51110689e42445046154abf
Fengyan Tina <alwarzontina @gmail.com>
2024-10-01 19:32:35 +0200

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-});
+    build: {
+        outDir: 'build', // Specify the output directory for the build
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              // Split node_modules into separate chunks
+              if (id.includes('node_modules')) {
+                return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+            },
+          },
+        },
+        chunkSizeWarningLimit: 1500, // Set a higher limit for chunk size warnings if needed
+      },
+    });
